51b1208d55369752e5d3121e1d93e3b8234dc498
Zennon <zennon.melnyk@gmail.com>
2020-10-28 22:36:38 -0400

    added notes

---
+++ Assets/Scripts/Interactables/Note.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : InteractComponent
+{
+    public GameManager manager;
+    public string noteText;
+
+    public override void Interact() {
+        manager.DisablePlayerMovement();
+        manager.LoadNote(noteText);
+    }
+}
