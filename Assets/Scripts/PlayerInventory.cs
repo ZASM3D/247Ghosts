f430bb77d42021df0d5db803c35912aea114e400
Zennon <zennon.melnyk@gmail.com>
2020-11-03 12:59:55 -0500

    Added script for managing inventory

---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    private HashSet<string> heldItems;
+
+    public void AddItem(string item) {
+        heldItems.Add(item);
+    }
+
+    public bool HasItem(string item) {
+        return heldItems.Contains(item);
+    }
+
+    public void RemoveItem(string item) {
+        heldItems.Remove(item);
+    }
+}
