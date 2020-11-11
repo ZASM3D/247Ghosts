0a992af6918f2a027aea281d2e42868595d05866
Elena Berman <eaberman3@gmail.com>
2020-11-11 12:20:45 -0600

    New objects and scripts for the puzzle

---
+++ Assets/Scripts/Interactables/SheetMusic.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheetMusic : InteractComponent
+{
+    private GameManager manager;
+
+    void Start() {
+        manager = GameState.Manager;
+    }
+
+    public override void Interact() {
+        manager.DisablePlayerMovement();
+
+    }
+}
