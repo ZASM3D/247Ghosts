6cd4f834612a79957414b65a5b8cf589ad277014
Elena Berman <eaberman3@gmail.com>
2020-11-12 09:23:06 -0600

    Text on mirror in Lily's room

---
+++ Assets/Mirror.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mirror : InteractComponent
+{
+	private GameManager manager;
+    public GameObject[] lilyText;
+    private int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject g in lilyText) {
+        	g.SetActive(false);
+        }
+        lilyText[0].SetActive(true);
+        index = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void Interact() {
+        manager.DisablePlayerMovement();
+        if (index > 0) {
+        	lilyText[index - 1].SetActive(false);
+        }
+    	lilyText[index].SetActive(true);
+    	index = (index + 1) % lilyText.Length;
+    }
+}
