d65d5d5ecf8cb83f44d8a596fe5c60e32b7e6e32
Elena Berman <eaberman3@gmail.com>
2020-11-14 08:30:08 -0600

    Dining room puzzle

---
+++ Assets/Scripts/PlaceSettingPuzzle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceSettingPuzzle : InteractComponent
+{
+    private GameManager manager;
+    public GameObject secret;
+    public GameObject prompt;
+    public GameObject foundSpoon;
+    public string requiredObject;
+
+    void Start() {
+        manager = GameState.Manager;
+        prompt.SetActive(true);
+        secret.SetActive(false);
+        foundSpoon.SetActive(false);
+    }
+
+    public override void Interact() {
+    	if (GameState.Manager.CheckPlayerHasItem(requiredObject)) {
+            GameState.Manager.RemoveItem(requiredObject);
+            Debug.Log("Unlocked with " + requiredObject);
+            foundSpoon.SetActive(true);
+            prompt.SetActive(false);
+        	secret.SetActive(true);
+        }
+    }
+}
