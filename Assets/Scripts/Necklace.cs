f406a7219b83440565de85d5b4044611b51c5026
Zennon <zennon.melnyk@gmail.com>
2020-11-14 22:19:52 -0500

    made the necklace teleport you to end scene

---
+++ Assets/Scripts/Necklace.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Necklace : InteractComponent
+{
+    public string nextScene;
+
+    void Start() {
+        Debug.Log(SceneManager.GetActiveScene().name);
+    }
+
+    public override void Interact() {
+        SceneManager.LoadScene(nextScene);
+    }
+}
