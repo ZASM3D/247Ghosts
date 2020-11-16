e72e3a312387803069b8f4ad9c5e67d15ac6d3e2
Zennon <zennon.melnyk@gmail.com>
2020-11-15 22:35:27 -0500

    added pedestals and 3-item bookcase

---
+++ Assets/Scripts/BookCase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookCase : MonoBehaviour
+{
+    public int unlocksNeeded;
+    private int numUnlocks = 0;
+    private bool open = false;
+    public Transform openLocation;
+
+    public void IncrementCount() {
+        numUnlocks++;
+        if (numUnlocks < unlocksNeeded || open)
+            return;
+
+        open = true;
+        this.transform.position = openLocation.position;
+        this.transform.rotation = openLocation.rotation;
+    }
+}
---
+++ Assets/Scripts/Interactables/ItemPedestal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPedestal : ObjectLock
+{
+    public BookCase bookCase;
+    public GameObject unlockItem;
+
+    void Start() {
+        unlockItem.SetActive(false);
+    }
+
+    public override void UnlockAction() {
+        unlockItem.SetActive(true);
+        bookCase.IncrementCount();
+    }
+}
