05583a62186102259ca80ee5433bfd679868caaf
Elena Berman <eaberman3@gmail.com>
2020-11-18 10:01:06 -0600

    New object script

---
+++ Assets/Scripts/Interactables/ObjectWithDialogue.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectWithDialogue : InteractComponent
+{
+    public GameObject dialogue;
+
+    public override void Interact() {
+        dialogue.SetActive(true);
+    }
+}
