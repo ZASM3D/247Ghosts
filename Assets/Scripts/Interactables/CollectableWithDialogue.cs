0968745281dffecfb10caf97d2d0b5041aa2d603
Elena Berman <eaberman3@gmail.com>
2020-11-18 09:41:06 -0600

    Added dialogue to objects

---
+++ Assets/Scripts/Interactables/CollectableWithDialogue.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableWithDialogue : InteractComponent
+{
+    public string item;
+    public GameObject dialogue;
+
+    public override void Interact() {
+        GameState.Manager.GivePlayerItem(item);
+        this.gameObject.SetActive(false);
+        dialogue.SetActive(true);
+    }
+}
